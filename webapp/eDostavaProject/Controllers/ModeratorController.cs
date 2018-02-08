51d81b37a026a59e8b2e6df5af81ab5318dc0088
eldar.jah <eldar.jah@gmail.com>
2018-02-08 23:10:00 +0100

    Moderator kontroler, pregled moderatora svih narucioca

---
+++ webapp/eDostavaProject/Controllers/ModeratorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using eDostava.Data;
+using eDostava.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace eDostava.Web.Controllers
+{
+    public class ModeratorController : Controller
+    {
+        private MojContext context;
+        public ModeratorController(MojContext db)
+        {
+            context = db;
+        }
+        public IActionResult Narucioci()
+        {
+            NarucilacPrikazVM Model = new NarucilacPrikazVM();
+            Model.Narucioci = context.Narucioci.Select(x => new NarucilacPrikazVM.NaruciociInfo()
+            {
+                Narucilac = x,
+                Ime = x.Ime,
+                Prezime = x.Prezime,
+                Username = x.Username,
+                Password = x.Password,
+                Email = x.Email,
+                Telefon = x.Telefon,
+                DatumKreiranja = x.DatumKreiranja.ToString(),
+                BlokNaziv = x.Blok.Naziv,
+                BadgeNaziv = x.Badge.Naziv,
+                GradNaziv = x.Blok.Grad.Naziv,
+                PostanskiBroj = x.Blok.Grad.PoštanskiBroj,
+            })
+            .ToList();
+
+            return View(Model);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ webapp/eDostavaProject/ViewModels/NarucilacPrikazVM.cs
@@ -0,0 +1,30 @@
+﻿using eDostava.Data.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDostava.Web.ViewModels
+{
+    public class NarucilacPrikazVM
+    {
+        public class NaruciociInfo
+        {
+            public Narucilac Narucilac;
+            public string Ime { get; set; }
+            public string Prezime { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Email { get; set; }
+            public string DatumKreiranja { get; set; }
+            public string Telefon { get; set; }
+            public string BadgeNaziv { get; set; }
+            public string BlokNaziv { get; set; }
+            public string GradNaziv { get; set; }
+            public int PostanskiBroj { get; set; }
+        }
+
+        public List<NaruciociInfo> Narucioci;
+    }
+}

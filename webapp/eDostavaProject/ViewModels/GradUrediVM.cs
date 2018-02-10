887a10770bc3d330e81f53289eac89594d06a39c
eldar.jah <eldar.jah@gmail.com>
2018-02-10 11:55:18 +0100

    Uradjen kontroler, viewovi i modeli za Grad

---
+++ webapp/eDostavaProject/ViewModels/GradUrediVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eDostava.Data.Models;
+
+namespace eDostava.Web.ViewModels
+{
+    public class GradUrediVM
+    {
+        public int GradId { get; set; }
+        public string Naziv { get; set; }
+        public int PostanskiBroj { get; set; }
+        public int BrojBlokova { get; set; }
+    }
+}

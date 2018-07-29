e01eda396ae785d1fdffcc00447f45837d13239f
eldar.jah <eldar.jah@gmail.com>
2018-07-29 21:33:30 +0200

    Izbrisao JS affix i dodao jednostavni BS position:sticky za Korpu.

--- webapp/eDostavaProject/wwwroot/js/site.js
+++ webapp/eDostavaProject/wwwroot/js/site.js
@@ -20,28 +20,8 @@ $(document).ready(function () {
             });
         })
 
-
-        // Korpa element
-        let $korpa = $(".korpa-outer-wrap");
-        $korpa.dynamicWidth = function () {
-            $korpa.css("width", $korpa[0].getBoundingClientRect().width);
-        }
-        $korpa.dynamicWidth();
-
-        let $smartAffixAElements = $('[data-smart-affix]');
-        $smartAffixAElements.each(function () {
-            console.log($(this).offset().top);
-            $(this).affix({
-                offset: {
-                    top: $(this).offset().top - 50,
-                }
-            })
-        })
         $(window).on("resize", function () {
             $korpa.dynamicWidth();
-            $smartAffixAElements.each(function () {
-                $(this).data('bs.affix').options.offset.top = $(this).offset().top - 50
-            })
         })
     });
 });

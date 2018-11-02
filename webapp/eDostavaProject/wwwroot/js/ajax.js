bba6b16ed9bb123df471115247cb98f399f71746
eldar.jah <eldar.jah@gmail.com>
2018-11-02 17:10:25 +0100

    Dodao custom JS i jQuery handlere, validatore isl.

--- webapp/eDostavaProject/wwwroot/js/ajax.js
+++ webapp/eDostavaProject/wwwroot/js/ajax.js
@@ -1,4 +1,16 @@
 ﻿function DodajAjaxEvente() {
+    // Onload ajax
+    $("[ajax-onload='da']").each(function (el) {
+        $(this).attr("ajax-onload", "dodan");
+
+        var urlZaPoziv = $(this).attr("ajax-url");
+        var divZaRezultat = $(this).attr("ajax-rezultat");
+
+        $.get(urlZaPoziv, function (data, status) {
+            $("#" + divZaRezultat).html(data);
+        });
+    });
+
     $("button[ajax-poziv='da']").click(function (event) {
         $(this).attr("ajax-poziv", "dodan");
 
@@ -17,6 +29,31 @@
         $("#bt").click(function (event) {
             ("#val").html("Opcija dopustena samo moderatoru");
 
+        });
+    });
+
+    $("[ajax-onchange='da']").off().on("change", function () {
+        let self = $(this);
+        var urlZaPoziv = self.attr("ajax-url");
+
+        if (typeof self.attr("ajax-add-value-param") != 'undefined') {
+            urlZaPoziv += "?" + self.attr("name") + "=" + self.val();
+        }
+
+        $.ajax({
+            type: "GET",
+            url: urlZaPoziv,
+            async: true,
+            success: function (data) {
+                self.removeClass('is-invalid');
+                self.addClass('is-valid');
+                $("#" + self.attr('id') + "Help").addClass('invisible');
+            },
+            error: function (data) {
+                self.removeClass('is-valid');
+                self.addClass('is-invalid');
+                $("#" + self.attr('id') + "Help").removeClass('invisible');
+            }
         });
     });
 
@@ -82,29 +119,65 @@
         });
     });
 
-    $("form[ajax-poziv='da']").submit(function (event) {
-        $(this).attr("ajax-poziv", "dodan");
-        event.preventDefault();
-        var urlZaPoziv1 = $(this).attr("ajax-url");
-        var urlZaPoziv2 = $(this).attr("action");
-        var divZaRezultat = $(this).attr("ajax-rezultat");
-
-        var urlZaPoziv;
-        if (urlZaPoziv1 instanceof String)
-            urlZaPoziv = urlZaPoziv1;
-        else
-            urlZaPoziv = urlZaPoziv2;
-
+    $("form[ajax-poziv='da']").off().submit(function (event) {
         var form = $(this);
+        var checkErrors = form.attr('check-for-errors-on-submit');
 
-        $.ajax({
-            type: "POST",
-            url: urlZaPoziv,
-            data: form.serialize(),
-            success: function (data) {
-                $("#" + divZaRezultat).html(data);
-            }
-        });
+        event.preventDefault();
+
+        var valid = true;
+        if (typeof checkErrors !== typeof undefined && checkErrors !== false) {
+            form.find('input').each(function () {
+                if ($(this).hasClass('is-invalid')) {
+                    // check for alertify message popups
+                    if (typeof form.attr('data-alertify') !== 'undefined') {
+                        let msg = '';
+                        if (form.attr('data-alertify-error')) {
+                            msg += `<span> ${form.attr('data-alertify-error')} </span>`;
+                        }
+                        alertify.log(msg);
+                    }
+                    valid = false;
+                    return false;
+                }
+            });
+        }
+        if (valid) {
+            form.attr("ajax-poziv", "dodan");
+            event.preventDefault();
+            var urlZaPoziv1 = form.attr("ajax-url");
+            var urlZaPoziv2 = form.attr("action");
+            var divZaRezultat = form.attr("ajax-rezultat");
+
+            var urlZaPoziv;
+            if (urlZaPoziv1 instanceof String)
+                urlZaPoziv = urlZaPoziv1;
+            else
+                urlZaPoziv = urlZaPoziv2;
+
+            $.ajax({
+                type: "POST",
+                url: urlZaPoziv,
+                data: form.serialize(),
+                success: function (data) {
+                    $("#" + divZaRezultat).html(data);
+                    // check for alertify message popups
+                    if (typeof form.attr('data-alertify') !== 'undefined') {
+                        let msg = '';
+                        if (form.attr('data-alertify-text')) {
+                            msg += `<p> ${form.attr('data-alertify-text')} </p>`;
+                        }
+                        if (form.attr('data-alertify-btn-pre-text')) {
+                            msg += `<span> ${form.attr('data-alertify-btn-pre-text')} </span>`;
+                        }
+                        if (form.attr('data-alertify-btn-text')) {
+                            msg += `<a class="btn btn-sm btn-danger center" href="form.attr('data-alertify-goto-url')}">${form.attr('data-alertify-btn-text')}</a>`;
+                        }
+                        alertify.log(msg);
+                    }
+                }
+            });
+        }
     });
 }
 $(document).ready(function () {
--- webapp/eDostavaProject/wwwroot/js/site.js
+++ webapp/eDostavaProject/wwwroot/js/site.js
@@ -1,6 +1,16 @@
 ﻿// Write your JavaScript code.
 $(document).ready(function () {
     $(function () {
+        // Live form edits (npr profil)
+        $("#korisnik-right-wrap").on('click', '.live-edit-section', function () {
+            $(this).closest(".form-group").toggleClass('edit');
+            if ($('.form-group.edit').length > 0) {
+                $("form.live-edit").addClass("edit");
+            } else {
+                $("form.live-edit").removeClass("edit")
+            }
+        });
+
         // Alertify messages 
         let $alertBtns = $("[data-alertify]");
         $alertBtns.each(function () {
@@ -20,8 +30,57 @@ $(document).ready(function () {
             });
         })
 
+        // Handle Korpa width on resize
         $(window).on("resize", function () {
             $korpa.dynamicWidth();
         })
     });
+
+    DodajHandlere();
+});
+
+function DodajHandlere() {
+    // Toggle password fields visibility
+    $(".see-password-input").off().on('click', function () {
+        let targetId = $(this).attr('data-target'),
+            targetEl = $('#' + targetId),
+            toggleIcon = $("[data-icon-for='" + targetId + "']");
+
+        if (targetEl.attr('type') === "password") {
+            targetEl.attr('type', 'text');
+            toggleIcon.removeClass("fa-eye")
+                .addClass("fa-eye-slash");
+        } else {
+            targetEl.attr('type','password');
+            toggleIcon.removeClass("fa-eye-slash")
+                .addClass("fa-eye");
+        }
+    });
+
+    // Check if fields are same
+    $("[data-check-is-same]").off().on("change", function () {
+        let checkAgainstId = $(this).attr('data-check-is-same'),
+            checkAgainstEl = $('#' + checkAgainstId),
+            checkErrorMsgEl = $('#' + $(this).attr('data-check-erorr-id'));
+
+        if ($(this).val() === checkAgainstEl.val()) {
+            checkErrorMsgEl.addClass('invisible');
+            $(this).removeClass('is-invalid')
+                   .addClass('is-valid');
+            checkAgainstEl.removeClass('is-invalid')
+                          .addClass('is-valid');
+        } else {
+            checkErrorMsgEl.removeClass('invisible');
+            $(this).removeClass('is-valid')
+                   .addClass('is-invalid');
+            checkAgainstEl.removeClass('is-valid')
+                          .addClass('is-invalid');
+        }
+    });
+}
+
+$(document).ajaxComplete(function () {
+    // izvršava nakon bilo kojeg ajax poziva
+    DodajHandlere();
+
 });

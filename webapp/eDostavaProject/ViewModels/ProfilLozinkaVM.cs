503e4e4abe794c7db00827bf19b2be7b9c44c8ec
eldar.jah <eldar.jah@gmail.com>
2018-11-02 17:09:53 +0100

    Dodao view i VM za izmjenu profil-lozinke.

---
+++ webapp/eDostavaProject/ViewModels/ProfilLozinkaVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDostava.Web.ViewModels
+{
+    public class ProfilLozinkaVM
+    {
+        public int KorisnikID { get; set; }
+        public string Password { get; set; }
+        public string NoviPassword { get; set; }
+        public string NoviPasswordPonovo { get; set; }
+    }
+}

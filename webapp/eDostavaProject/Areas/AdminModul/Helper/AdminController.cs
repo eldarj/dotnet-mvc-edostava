ab5caffb6bb3d465fdeee64433026497f9481c4c
eldar.jah <eldar.jah@gmail.com>
2018-11-08 13:35:08 +0100

    Dodao AdminController base da bi svi controlleri iz AdminModula mogli samo jednostavno naslijediti Area i Filter atribut.

---
+++ webapp/eDostavaProject/Areas/AdminModul/Helper/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using eDostava.Web.Areas.AdminModul.Filters;
+
+namespace eDostava.Web.Areas.AdminModul.Helper
+{
+    [Area("AdminModul")]
+    [Auth(moderator: true)]
+    public class AdminController : Controller
+    {
+
+    }
+}

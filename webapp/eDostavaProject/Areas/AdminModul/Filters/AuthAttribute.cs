e14e32e4fb5f0e296f3a0b31803c64c022faaba0
eldar.jah <eldar.jah@gmail.com>
2018-11-08 13:34:01 +0100

    Dodao AuthFilter za AdminModul.

---
+++ webapp/eDostavaProject/Areas/AdminModul/Filters/AuthAttribute.cs
@@ -0,0 +1,62 @@
+﻿using eDostava.Data;
+using eDostava.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using RS1_Ispit_2017.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDostava.Web.Areas.AdminModul.Filters
+{
+    public class AuthAttribute : TypeFilterAttribute
+    {
+        public AuthAttribute(bool moderator) : base(typeof(Auth))
+        {
+            Arguments = new object[] { moderator };
+        }
+    }
+
+    public class Auth : IAsyncActionFilter
+    {
+        private readonly bool _moderator;
+
+        public Auth(bool moderator)
+        {
+            _moderator = moderator;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Moderator moderator = Autentifikacija.GetLogiranogModeratora(context.HttpContext);
+
+            if (moderator == null)
+            {
+                ErrorAndRedirect(context);
+                return;
+            }
+
+            MojContext db = context.HttpContext.RequestServices.GetService<MojContext>();
+
+            if (_moderator && db.Moderatori.Any(m => m.KorisnikID == moderator.KorisnikID))
+            {
+                await next();
+                return;
+            }
+
+            ErrorAndRedirect(context);
+        }
+
+        private void ErrorAndRedirect(ActionExecutingContext context)
+        {
+            if (context.Controller is Controller controller)
+            {
+                controller.TempData["Error"] = "Nemate pravo pristupa!";
+            }
+
+            context.Result = new RedirectToActionResult("Index", "Login", new { area = "" });
+        }
+    }
+}

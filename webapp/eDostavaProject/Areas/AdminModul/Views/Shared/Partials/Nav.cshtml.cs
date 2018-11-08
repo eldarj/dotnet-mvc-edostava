cc35f9abe17b34b0304b67afd02e9c19c4e1dca4
eldar.jah <eldar.jah@gmail.com>
2018-11-08 10:42:51 +0100

    Dodao sve viewove za AdminAreu.

---
+++ webapp/eDostavaProject/Areas/AdminModul/Views/Shared/Partials/Nav.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eDostava.Web.Areas.AdminModul.Views.Shared.Partials
+{
+    public class NavModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

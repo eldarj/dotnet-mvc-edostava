f69fb86af61df3fc0bb0450de7b1ba55822839fc
eldar.jah <eldar.jah@gmail.com>
2018-11-29 11:36:25 +0100

    Added session ctrl and views, within admin area.

---
+++ webapp/eDostavaProject/Areas/AdminModul/Controllers/SessionController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using eDostava.Web.Areas.AdminModul.ViewModels;
+using eDostava.Data;
+using eDostava.Web.Areas.AdminModul.Helper;
+using RS1_Ispit_2017.Helper;
+using eDostava.Data.Models;
+
+namespace eDostava.Web.Areas.AdminModul.Controllers
+{
+    public class SessionController : AdminController
+    {
+        private MojContext context;
+
+        public SessionController(MojContext db)
+        {
+            context = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(PrepareAllSession());
+        }
+
+        public IActionResult Obrisi(string token)
+        {
+            AuthToken x = context.AuthTokeni.Where(t => t.Value == token).FirstOrDefault();
+
+            context.AuthTokeni.Remove(x);
+            context.SaveChanges();
+
+            return PartialView("Index", PrepareAllSession());
+        }
+
+        private SessionPrikazVM PrepareAllSession()
+        {
+            return new SessionPrikazVM
+            {
+                Sessions = context.AuthTokeni
+                .Select(t => new SessionPrikazVM.Session
+                {
+                    Ip = t.Ip,
+                    LoginTime = t.DatumGenerisanja,
+                    Token = t.Value,
+                })
+                .ToList(),
+                TrenutniToken = Autentifikacija.GetCurrentToken(HttpContext)
+            };
+        }
+    }
+}
---
+++ webapp/eDostavaProject/Areas/AdminModul/ViewModels/Session/SessionPrikazVM.cs
@@ -0,0 +1,24 @@
+﻿using eDostava.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDostava.Web.Areas.AdminModul.ViewModels
+{
+    public class SessionPrikazVM
+    {
+
+        public class Session
+        {
+            public string Ip { get; set; }
+            public string Token { get; set; }
+            public string Username { get; set; }
+            public DateTime LoginTime { get; set; }
+        }
+
+        public List<Session> Sessions { get; set; }
+        public string TrenutniToken { get; set; }
+
+    }
+}

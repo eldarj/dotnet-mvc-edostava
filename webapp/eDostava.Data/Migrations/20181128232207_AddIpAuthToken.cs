d1634bd5c17474374f065d6d14fa0ed1af5b8491
eldar.jah <eldar.jah@gmail.com>
2018-11-29 11:34:37 +0100

    Added auth token model.

---
+++ webapp/eDostava.Data/Migrations/20181128232207_AddIpAuthToken.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace eDostava.Data.Migrations
+{
+    public partial class AddIpAuthToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ip",
+                table: "AuthTokeni",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ip",
+                table: "AuthTokeni");
+        }
+    }
+}
--- webapp/eDostava.Data/Models/AuthToken.cs
+++ webapp/eDostava.Data/Models/AuthToken.cs
@@ -9,10 +9,34 @@ namespace eDostava.Data.Models
     {
         public int Id { get; set; }
         public string Value { get; set; }
-        [ForeignKey(nameof(Korisnik))]
-        public int KorisnikId { get; set; }
-        public Korisnik Korisnik { get; set; }
         public DateTime DatumGenerisanja { get; set; }
+        public string Ip { get; set; }
+
+        // FKs - check this
+        [ForeignKey(nameof(VlasnikId))]
+        public int? VlasnikId { get; set; }
+        public virtual Vlasnik Vlasnik { get; set; }
+
+        [ForeignKey(nameof(NarucilacId))]
+        public int? NarucilacId { get; set; }
+        public virtual Narucilac Narucilac { get; set; }
+
+        [ForeignKey(nameof(ModeratorId))]
+        public int? ModeratorId { get; set; }
+        public virtual Moderator Moderator { get; set; }
+
+        // check this
+        public string GetKorisnikUsername()
+        {
+            foreach (var item in new List<Korisnik> { (Korisnik)Vlasnik, (Korisnik)Narucilac, (Korisnik)Moderator })
+            {
+                if (item != null)
+                {
+                    return item.Username;
+                }
+            }
+            return "-";
+        }
 
     }
 }

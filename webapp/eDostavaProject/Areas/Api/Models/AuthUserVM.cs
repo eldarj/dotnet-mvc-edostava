0980f9ee5a1b7121943deb3e82ad783c8601a32d
eldar.jah <eldar.jah@gmail.com>
2018-12-01 12:05:29 +0100

    Add password to auth controller and model.

--- webapp/eDostavaProject/Areas/Api/Models/AuthUserVM.cs
+++ webapp/eDostavaProject/Areas/Api/Models/AuthUserVM.cs
@@ -10,6 +10,7 @@ namespace eDostava.Web.Areas.Api.Models
     {
         public int Id { get; set; }
         public string Username { get; set; }
+        public string Password { get; set; }
         public string Ime { get; set; }
         public string Prezime { get; set; }
         public string Adresa { get; set; }

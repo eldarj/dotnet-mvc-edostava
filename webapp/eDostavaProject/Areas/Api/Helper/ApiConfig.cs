6fa28e8227a9f81cc697a239f18fc2e5d69ba8c3
eldar.jah <eldar.jah@gmail.com>
2018-12-02 10:49:27 +0100

    Adding helper config.

---
+++ webapp/eDostavaProject/Areas/Api/Helper/ApiConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDostava.Web.Areas.Api.Helper
+{
+    public static class ApiConfig
+    {
+        public static readonly string IMAGE_DIR = "uploads/images/korisnik";
+
+        public static readonly string DEFAULT_IMAGE = "default_identicon.png";
+    }
+}

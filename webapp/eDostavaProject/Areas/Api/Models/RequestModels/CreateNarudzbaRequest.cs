a260e4538830386016b7580d6f987645545ccb3c
eldar.jah <eldar.jah@gmail.com>
2018-12-03 22:11:18 +0100

    Adding create action and request model for narudzba.

---
+++ webapp/eDostavaProject/Areas/Api/Models/RequestModels/CreateNarudzbaRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDostava.Web.Areas.Api.Models.RequestModels
+{
+    public class CreateNarudzbaRequest
+    {
+        public UserLoginRequest credentials { get; set; }
+
+        public class StavkaRequest
+        {
+            public int HranaID { get; set; }
+            public int Kolicina { get; set; }
+        }
+        public double UkupnaCijena { get; set; }
+        public List<StavkaRequest> stavke { get; set; }
+
+    }
+}

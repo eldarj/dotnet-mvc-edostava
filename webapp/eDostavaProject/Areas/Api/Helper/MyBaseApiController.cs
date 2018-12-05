1a096c014b599506f3db3f1910f29489dc8c05cd
eldar.jah <eldar.jah@gmail.com>
2018-12-05 18:51:07 +0100

    Created base controller for api.

---
+++ webapp/eDostavaProject/Areas/Api/Helper/MyBaseApiController.cs
@@ -0,0 +1,22 @@
+﻿using eDostava.Data;
+using eDostava.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eDostava.Web.Areas.Api.Helper
+{
+    public class MyBaseApiController : Controller
+    {
+        protected readonly MojContext _context;
+
+        protected MyBaseApiController(MojContext db)
+        {
+            _context = db;
+        }
+
+        protected Narucilac MyAuthUser => HttpContext.GetKorisnikOfAuthToken();
+    }
+}
